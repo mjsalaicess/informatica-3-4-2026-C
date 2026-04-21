d87d715e75d4fada182894fc7efc216f4aa1a352
mjsalaicess <mjsalaicess@gmail.com>
2026-04-21 21:48:38 +0000

    curly pancake

---
+++ naming-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    //I deleted the exclamation mark.
+    int test_value;
+    //I deleted the space and replaced it with an underscore.
+    int example1;
+    //I moved the number one from the front to the back of the word.
+    float maria;
+    //we deleted the "int" and replaced it with a name. 
+    printf("Variables created successfully.\n");
+}
