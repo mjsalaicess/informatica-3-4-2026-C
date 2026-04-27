603348e16180033978e377df21d219fe1c1aa6e0
mjsalaicess <mjsalaicess@gmail.com>
2026-04-27 17:19:13 +0000

    this class was cool

--- temperature.c
+++ temperature.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main (void) {
-    int Fahrenheit;
-    int Celsius;
+    float Fahrenheit;
+    float Celsius;
     printf("Enter Temperature in Fahrenheit:" );
-    scanf(")
+    scanf("%f", &Fahrenheit);
+    Celsius= (Fahrenheit-32)/1.8;
+    printf("%.2f Degrees Fahrenheit it's equal to %.2f Degres Celsius \n",Fahrenheit, Celsius);
+    //-40 Degrees Fahrenheit is equal to -40 Degrees Celsius
 }
