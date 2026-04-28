06bc330b12095be958f214e1f7445a3524c362ac
mjsalaicess <mjsalaicess@gmail.com>
2026-04-28 21:59:56 +0000

    g

---
+++ minutes.c
@@ -0,0 +1,10 @@
+#include <stdio>
+int main(void){
+    int movie in minutes;
+    int start;
+    printf ("Movie Running Time (in minutes)");
+    scanf ("%d", &movie in minutes);
+    printf ("Start Time");
+    scanf ("%d", &start);
+    
+}
