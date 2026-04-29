9d85b141f5edcb16b155b0aa1a752488ae8a6d37
mjsalaicess <mjsalaicess@gmail.com>
2026-04-29 17:28:52 +0000

    ugh.test

---
+++ test/receipt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    int main (void){
+    char item1 = 'O';
+    printf("Item Name:" , item1/n);
+    scanf("%d", &item1);
+
+    float price1 = 3.99;
+    printf("Price:%.2f", price1/n);
+    scanf("%d", &price1);
+
+    int quantity1 = 3;
+    printf("Quantity Bought:" , quantity1/n);
+    scanf("%d", &quantity1);
+
+}
