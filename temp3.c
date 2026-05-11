19ddc3ccac4ffe4d97d07eb72f8d32998178ca19
mjsalaicess <mjsalaicess@gmail.com>
2026-05-11 17:29:06 +0000

    i lwk didnt know this

---
+++ temp3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//1.Prototype
+void c_to_f(void);
+void f_to_c(void);
+
+int main(void){
+    int user_response;
+    printf("Temperature Converter\n");
+    printf("Select Option\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    scanf ("%d", &user_response);
+
+    if(user_response ==1)
+}
